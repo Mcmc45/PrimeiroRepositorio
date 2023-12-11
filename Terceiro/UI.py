12a85bd31a4413022cd12c9c5ce4e95efe4b1dce
Maria Clara <moraesmc70@gmail.com>
2023-12-11 15:04:48 -0300

    Segundo commit

--- Terceiro/UI.py
+++ Terceiro/UI.py
@@ -4,5 +4,5 @@ window.title('TKinter Py App')
 window.geometry('300x300')
 newLabel = tk.Label(text = 'Hello , TKInter!')
 newLabel.grid(column= 1, row= 3)
-
+newLabel.pack(anchor = 'center')
 window.mainloop()
